6d0e3276bb8bb595dfce81fd10a7ef2c7969fdb2
youpy <youpy@buycheapviagraonlinenow.com>
2010-02-16 10:43:15 +0900

    add spec for MDQueryNative

--- spec/md_query_native_spec.rb
+++ spec/md_query_native_spec.rb
@@ -8,4 +8,9 @@ describe Spotlight::MDQueryNative do
   it "should instanciate" do
     @query.should be_an_instance_of(Spotlight::MDQueryNative)
   end
+
+  it "should execute" do
+    @query.set_search_scopes([File.expand_path(File.dirname(__FILE__))]);
+    @query.execute.first.should be_an_instance_of(Spotlight::MDItemNative)
+  end
 end
